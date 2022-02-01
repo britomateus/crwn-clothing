551530a0e6ab45bb9585a00b6bf10e04ec77c083
Mateus Brito <mateusbrito4@gmail.com>
2022-02-01 12:52:05 -0300

    implemented firebase utils, added sign up component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Route, Switch } from 'react-router-dom'
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
 import SignInSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component'
-import { auth } from './firebase/firebase.utils'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 
 class App extends Component {
   constructor(props) {
@@ -20,10 +20,23 @@ class App extends Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth)
 
-      console.log(user)
+        userRef.onSnapshot((snapshot) => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data(),
+            },
+          })
+
+          console.log(this.state)
+        })
+      }
+
+      this.setState({ currentUser: userAuth })
     })
   }
 
